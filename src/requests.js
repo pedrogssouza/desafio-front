8447e722205f48f57d9e2111b98000598da4bffd
Pedro Gomes <pgsantanadesouza@usp.br>
2021-10-02 18:02:35 -0300

    Rotas configuradas para o backend

---
+++ src/requests.js
@@ -0,0 +1,44 @@
+export async function postRequest(endPoint, data) {
+  const response = await fetch(
+    "https://sistema-cobranca-localhost071.herokuapp.com" + endPoint,
+    {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  return response;
+}
+
+export async function postProtectedRequest(endPoint, method, data, token) {
+  const response = await fetch(
+    "https://sistema-cobranca-localhost071.herokuapp.com" + endPoint,
+    {
+      method: method,
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response;
+}
+
+export async function getProtectedRequest(endPoint, token) {
+  const response = await fetch(
+    "https://sistema-cobranca-localhost071.herokuapp.com" + endPoint,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response;
+}
