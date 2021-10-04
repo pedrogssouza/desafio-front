b296d34bf7d4f2e82129e860d02f891cdd66a913
Pedro Gomes <pgsantanadesouza@usp.br>
2021-10-03 23:18:28 -0300

    add: componentes da homepage prontos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,61 @@
 import "./styles.css";
+import clients from "../../assets/clients.svg";
+import charges from "../../assets/charges.svg";
+import CountsContainer from "../../components/HomeCountComponent/index.js";
 
 function Home() {
-  return <div className="home-content"></div>;
+  const containers = [
+    {
+      title: "Clientes",
+      img: clients,
+      array: [
+        {
+          color: "green",
+          type: "Em dia",
+          count: 0,
+        },
+        {
+          color: "red",
+          type: "Inadimplentes",
+          count: 0,
+        },
+      ],
+    },
+    {
+      title: "Cobranças",
+      img: charges,
+      array: [
+        {
+          color: "blue",
+          type: "Previstas",
+          count: 0,
+        },
+        {
+          color: "red",
+          type: "Vencidas",
+          count: 0,
+        },
+        {
+          color: "green",
+          type: "Pagas",
+          count: 0,
+        },
+      ],
+    },
+  ];
+  return (
+    <div className="home-content">
+      {containers.map((container) => {
+        return (
+          <CountsContainer
+            title={container.title}
+            array={container.array}
+            img={container.img}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default Home;
